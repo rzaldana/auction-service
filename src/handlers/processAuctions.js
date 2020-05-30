178395b1c8fdb709eb7d5b8b7271a78027a121eb
Raul Zaldana <raularmandozc12@gmail.com>
2020-05-29 22:25:29 -0400

    implemented closing auctions

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,5 +1,20 @@
+import { getEndedAuctions } from "../lib/getEndedAuctions";
+import { closeAuction } from "../lib/closeAuction";
+import createError from "http-errors";
+
 async function processAuctions(event, context) {
   console.log("processing auctions!");
+  try {
+    const auctionsToClose = await getEndedAuctions();
+    const closePromises = auctionsToClose.map((auction) =>
+      closeAuction(auction)
+    );
+    await Promise.all(closePromises);
+    return { closed: auctionsToClose };
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 }
 
 export const handler = processAuctions;
